e5373ce6c074a8e54ce4d11f9914aebc6b2fa07d
ubuntumiki <ubuntu.miki@gmail.com>
2025-12-22 18:32:45 +0200

    Initial commit

---
+++ src/core/com/module_06/Level.java
@@ -0,0 +1,66 @@
+package core.com.module_06;
+
+class Level {
+    private Point[] points;
+    private LevelInfo levelInfo;
+
+    public Level(LevelInfo levelInfo, Point[] points) {
+        this.levelInfo = levelInfo;
+        this.points = points;
+    }
+
+    @Override
+    public String toString() {
+        return "Quarke level, name is " + levelInfo.name + ", difficulty is " + levelInfo.getDifficulty() + ", point count is " + points.length;
+    }
+
+    static class Point {
+        private int x, y;
+
+        public Point(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+
+        public int getX() {
+            return x;
+        }
+
+        public int getY() {
+            return y;
+        }
+    }
+
+    static class LevelInfo {
+        private String name;
+        private String difficulty;
+
+        public LevelInfo(String name, String difficulty) {
+            this.name = name;
+            this.difficulty = difficulty;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public String getDifficulty() {
+            return difficulty;
+        }
+    }
+}
+
+class LevelTest {
+    public static void main(String[] args) {
+        Level.Point p1 = new Level.Point(3, 5);
+        Level.Point p2 = new Level.Point(10, 100);
+        Level.Point p3 = new Level.Point(50, 40);
+
+        Level.Point[] points = {p1, p2, p3};
+
+        Level.LevelInfo info = new Level.LevelInfo("Quarke Intro", "Easy");
+
+        //Quarke level, name is Quarke Intro, difficulty is Easy, point count is 3
+        System.out.println(new Level(info, points));
+    }
+}
