86351311be0ff80c88b5b72d93ff087944c44b28
ubuntumiki <ubuntu.miki@gmail.com>
2025-12-25 11:49:30 +0200

    test Arrays.toString part2

--- src/core/com/test/GoItTest.java
+++ src/core/com/test/GoItTest.java
@@ -7,5 +7,11 @@ public class GoItTest {
         String dev = "I want to be a Java Developer";
 
         System.out.println(Arrays.toString(dev.getBytes()));
+
+        String love = "I love my Lyolya (-;";
+        System.out.println(Arrays.toString(love.getBytes()));
+
+        byte[] loveBytes = {73, 32, 108, 111, 118, 101, 32, 109, 121, 32, 76, 121, 111, 108, 121, 97, 32, 40, 45, 59};
+        System.out.println(new String(loveBytes));
     }
 }
