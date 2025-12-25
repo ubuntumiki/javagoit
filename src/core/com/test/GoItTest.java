1a88c6c03576f6b03af1aa0651587a377930385a
ubuntumiki <ubuntu.miki@gmail.com>
2025-12-25 11:45:28 +0200

    test Arrays.toString

---
+++ src/core/com/test/GoItTest.java
@@ -0,0 +1,11 @@
+package core.com.test;
+
+import java.util.Arrays;
+
+public class GoItTest {
+    public static void main(String[] args) {
+        String dev = "I want to be a Java Developer";
+
+        System.out.println(Arrays.toString(dev.getBytes()));
+    }
+}
