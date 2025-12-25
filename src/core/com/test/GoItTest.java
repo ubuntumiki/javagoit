e8389f3f13f29d65e75d16f9a09201494c2c8762
ubuntumiki <ubuntu.miki@gmail.com>
2025-12-25 12:40:37 +0200

    test StringBuilderTest

--- src/core/com/test/GoItTest.java
+++ src/core/com/test/GoItTest.java
@@ -10,6 +10,7 @@ public class GoItTest {
 
         String love = "I love my Lyolya (-;";
         System.out.println(Arrays.toString(love.getBytes()));
+        System.out.println(Arrays.toString(love.toCharArray()));
 
         byte[] loveBytes = {73, 32, 108, 111, 118, 101, 32, 109, 121, 32, 76, 121, 111, 108, 121, 97, 32, 40, 45, 59};
         System.out.println(new String(loveBytes));
