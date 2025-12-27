bec859127f24c9890ebb477b26f3fc5e907a0dda
ubuntumiki <ubuntu.miki@gmail.com>
2025-12-27 20:29:51 +0200

    class ArrayCleanerTests 2

--- src/core/com/module_7/ArrayCleanerTests.java
+++ src/core/com/module_7/ArrayCleanerTests.java
@@ -1,6 +1,8 @@
 package core.com.module_7;
 
 import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class ArrayCleanerTests {
     public static void main(String[] args) {
@@ -8,8 +10,13 @@ public class ArrayCleanerTests {
         String[] items = {"a", "b", "a", "c", "b", "a"};
 
         //Expected output data - a, b, c
-        String[] result = new ArrayCleaner().removeDuplicates(items);
+//        String[] result = new ArrayCleaner().removeDuplicates(items);
+//
+//        System.out.println(Arrays.toString(result));
 
-        System.out.println(Arrays.toString(result));
+        List<String> unique = Arrays.stream(items).distinct().toList();
+        String[] uniqueArray = unique.toArray(new String[0]);
+
+        System.out.println(Arrays.toString(uniqueArray));
     }
 }
