335de77d09bcec9617c8d44e0d3496e3173fe5ef
ubuntumiki <ubuntu.miki@gmail.com>
2025-12-29 12:02:17 +0200

    fix

--- src/core/com/module_7/ArrayCleanerTests.java
+++ src/core/com/module_7/ArrayCleanerTests.java
@@ -1,8 +1,8 @@
 package core.com.module_7;
 
 import java.util.Arrays;
+import java.util.LinkedHashSet;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class ArrayCleanerTests {
     public static void main(String[] args) {
@@ -14,9 +14,14 @@ public class ArrayCleanerTests {
 //
 //        System.out.println(Arrays.toString(result));
 
+        //Using Stream API
         List<String> unique = Arrays.stream(items).distinct().toList();
         String[] uniqueArray = unique.toArray(new String[0]);
 
         System.out.println(Arrays.toString(uniqueArray));
+
+        //Using LinkedHashSet
+        String[] uniqueItems2 = new LinkedHashSet<>(Arrays.asList(items)).toArray(new String[0]);
+        System.out.println(Arrays.toString(uniqueItems2));
     }
 }
