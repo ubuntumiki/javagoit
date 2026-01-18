31a11cf1a04e798c62c1faf2afdd49507b32bdbd
ubuntumiki <ubuntu.miki@gmail.com>
2026-01-18 10:26:46 +0200

    update module 11

---
+++ src/core/com/module_11/srteam/StreamApiMain.java
@@ -0,0 +1,20 @@
+package core.com.module_11.srteam;
+
+import java.util.List;
+import java.util.stream.IntStream;
+
+public class StreamApiMain {
+    public static void main(String[] args) {
+        IntStream intStream = IntStream.range(0, 100);
+
+        IntStream s = intStream
+                .filter(n -> n % 2 == 0)
+                .limit(10)
+                .skip(5)
+                .map(n -> n * 10);
+
+        List<Integer> list = s.boxed().toList();
+
+        System.out.println(list);
+    }
+}
