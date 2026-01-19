9d3068db28f4c29c0f53faf2eef52c3567c6911b
ubuntumiki <ubuntu.miki@gmail.com>
2026-01-19 22:06:23 +0200

    add module 12

--- src/core/com/module_11/srteam/StreamApiMain.java
+++ src/core/com/module_11/srteam/StreamApiMain.java
@@ -1,6 +1,7 @@
 package core.com.module_11.srteam;
 
 import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class StreamApiMain {
@@ -13,8 +14,11 @@ public class StreamApiMain {
                 .skip(5)
                 .map(n -> n * 10);
 
-        List<Integer> list = s.boxed().toList();
+//        List<Integer> list = s.boxed().toList();
+        List<Integer> list2 = s.boxed().collect(Collectors.toList());
 
-        System.out.println(list);
+
+//        System.out.println(list);
+        System.out.println(list2);
     }
 }
